8b0477ff5080f2bd8f1dd2c05c8583319750d022
arnaud.monteils@mnhn.fr <arnaud.monteils@mnhn.fr>
2022-02-06 16:31:48 +0100

    feat: adding mongoose

--- second_test/courier-capacity-manager/src/app.ts
+++ second_test/courier-capacity-manager/src/app.ts
@@ -1,13 +1,16 @@
+import bodyParser from "body-parser";
+import "dotenv/config";
 import express from "express";
+import connect from "./connect";
+export const app = express()
 
-export const app = express();
+const port = 3000;
 
-const port = 8080;
+app.use(bodyParser.json());
+app.listen(port, () => {console.log(`Now listenning on port ${port}`)});
 
-app.get("/", (req, res) => {
-  res.send("Dockerize the node app");
-});
-
-app.listen(port, () => {
-  console.log(`Now listenning on port ${port}`);
-});
+if(process.env.DB_URI) {
+  connect({uri: process.env.DB_URI, user: process.env.temp_passwd, pass: process.env.temp_passwd})
+} else {
+  console.error("Can not find db URI")
+}
