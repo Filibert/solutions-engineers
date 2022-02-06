b1ce7fd0df3b520d8a59f724b92958dcae0b23bb
arnaud.monteils@mnhn.fr <arnaud.monteils@mnhn.fr>
2022-02-06 13:47:30 +0100

    feat: add Dockerfile to serve the express api

--- second_test/courier-capacity-manager/src/app.ts
+++ second_test/courier-capacity-manager/src/app.ts
@@ -1,7 +1,13 @@
 import express from "express";
 
-export const app = express()
+export const app = express();
 
-const port = 3000;
+const port = 8080;
 
-app.listen(port, () => {console.log(`Now listenning on port ${port}`)});
+app.get("/", (req, res) => {
+  res.send("Dockerize the node app");
+});
+
+app.listen(port, () => {
+  console.log(`Now listenning on port ${port}`);
+});
